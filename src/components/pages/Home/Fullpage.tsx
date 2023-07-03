31a2742a4c45621b6d889339bd1d88620aed92cf
ConstIX <x.dilshd03@gmail.com>
2023-07-03 20:30:27 +0300

    modified:   package.json 	new file:   src/assets/img/logo-main.svg 	new file:   src/components/pages/Home/Fullpage.tsx 	modified:   src/components/pages/Home/index.tsx 	modified:   src/scss/scss-components/_combine.scss 	new file:   src/scss/scss-components/_fullpage.scss 	modified:   yarn.lock

---
+++ src/components/pages/Home/Fullpage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import image1 from '../../../assets/img/categories/01.jpg'
+import image2 from '../../../assets/img/categories/02.jpg'
+import image3 from '../../../assets/img/categories/03.jpg'
+import image4 from '../../../assets/img/categories/04.jpg'
+import logo from '../../../assets/img/logo-main.svg'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
+const fullscreen = [image1, image2, image3, image4]
+
+const Fullpage: React.FC = () => {
+   return (
+      <section className="fullpage">
+
+         <Swiper
+            modules={[EffectFade, Autoplay]}
+            effect={'fade'}
+            autoplay={{ delay: 4000, disableOnInteraction: false }}
+            className="fullpage__swiper"
+         >
+            {fullscreen.map((i, idx) => <SwiperSlide key={idx}><img src={i} alt="..." /></SwiperSlide>)}
+         </Swiper>
+
+         <div className="fullpage__body">
+            <div className="fullpage__item">
+               <div className="fullpage__image"><img src={logo} alt="" /></div>
+               <div className="fullpage__title">Реализуем проекты любой сложности – от стандартных бюджетных конструкций до концептуальных пространственных решений.</div>
+               <div className="btn btn_h fullpage__btn">заказать звонок</div>
+            </div>
+         </div>
+
+      </section>
+   )
+}
+
+export default Fullpage
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import Brand from './Brand'
 import Contacts from './Contacts'
 import DecorationCategory from "./DecorationCategory"
 import Features from "./Features"
+import Fullpage from './Fullpage'
 import Offer from "./Offer"
 import Portfolio from "./Portfolio"
 import WorkType from './WorkType'
@@ -11,6 +12,7 @@ import WorkType from './WorkType'
 const Home: React.FC = () => {
    return (
       <>
+         <Fullpage />
          <DecorationCategory />
          <Offer />
          <Features />
