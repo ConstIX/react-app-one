05c74640585298e5c5d551b53fca816d1b0e519e
ConstIX <x.dilshd03@gmail.com>
2023-07-05 13:35:53 +0300

    modified:   package.json 	modified:   src/components/pages/Home/Contacts/MessageBlock.tsx 	modified:   src/scss/_forms.scss 	modified:   yarn.lock

--- src/components/pages/Home/Contacts/MessageBlock.tsx
+++ src/components/pages/Home/Contacts/MessageBlock.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
 const MessageBlock: React.FC = () => {
+
    return (
       <div className="contacts__body">
 
          <div className="contacts__header-block header-block">
             <div className="header-block__title">Отправить сообщение</div>
          </div>
-
-         <div className="contacts__form"></div>
 
       </div>
    )
